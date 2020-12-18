26152238e74f338cdfdb3566c1c0622b478e1006
Markos Rafael <escalante.markos18@gmail.com>
2020-12-17 21:14:28 -0800

    Package json finally loaded into application. Update README and finish code for app.js. Test's are currently not working.

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
 # employeeTemplate
+
+## Description
+
+The purpose of this command line application is to dynamically create a software engineering team! 
+
+## Installation
+
+Use the package manager's [inquirer](https://www.npmjs.com/package/inquirer) and [jest](https://www.npmjs.com/package/jest) to install this application.
+```
+node i inquirer
+node i jest
+```
+![temp](https://user-images.githubusercontent.com/71291602/101871189-fe435e00-3b37-11eb-9f86-1c4daebb8599.gif)
+
+[video link](https://drive.google.com/file/d/1H__u1tlCO2zFJSqFmaX7oRgCvOBPkJWW/view)
+
+![screenshot](./img/commandline.jpg)
+
+## License
+
+[MIT](https://choosealicense.com/licenses/mit/)
--- app.js
+++ app.js
@@ -9,3 +9,79 @@ const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
+
+const employees = [];
+
+const questions = ([
+    {
+        type: "input",
+        messgae: "Let's build your software engineering team! Please enter the employee's name:",
+        name: "name",
+    },
+    {
+        type: "input",
+        message: "Please enter the employee's email:",
+        name: "email",
+    },
+    {
+        type: "input",
+        message: "Please enter the employee's ID #:",
+        name: "id",
+    },
+    {
+        type: "list",
+        message: "Please select the employee's role from the list below",
+        choices: [
+            "Manager",
+            "Engineer",
+            "Intern",
+        ],
+        name: "role",
+    },
+    {
+        type: "input",
+        message: "Please enter the manager's office number:",
+        name: "managerOffice",
+        when: (data) => data.role === "Manager",
+    },
+    {
+        type: "input",
+        message: "Please enter the engineer's github username:",
+        name: "github",
+        when: (data) => data.role === "Engineer",
+    },
+    {
+        type: "input",
+        message: "Please enter the intern's school name:",
+        name: "school",
+        when: (data) => data.role === "Intern",
+    }
+]);
+function init() {
+    inquirer.prompt(questions).then((data) => {
+        if (data.role === "Manager") {
+            const manager = new Manager(data.name, data.id, data.email, data.managerOffice);
+            employees.push(manager);
+        } else if (data.role === "Engineer") {
+            const engineer = new Engineer(data.name, data.id, data.email, data.github);
+            employees.push(engineer);
+        } else if (data.role === "Intern") {
+            const intern = new Intern(data.name, data.id, data.email, data.school);
+            employees.push(intern);
+        } else {
+            console.log("Please select a role from the provided list.")
+        }
+        
+    })
+}
+
+function createTeam() {
+    const member = render(employees)
+    fs.writeFile(outputPath, member, (err) => {
+        if (err) throw err;
+        console.log("Your team has been built!")
+    })
+}
+
+init();
+createTeam();
